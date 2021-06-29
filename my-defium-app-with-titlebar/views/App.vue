1ab67f7183cf4b7b4be1a21d8da10af39784ae3e
odd86 <odd.jorgen.roland@gmail.com>
2021-06-29 13:08:55 +0200

    Add files via upload

---
+++ my-defium-app-with-titlebar/views/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <window class="wallpaper">
+    <div>
+	<span>Your app goes here</span>
+	<div>{{function1(2) }} </div> <!-- Will output 4 -->
+	</div>
+  </window>
+</template>
+
+<script>
+import window from '@/os/components/window/Window.vue';
+
+export default {
+  name: 'MyDefiumApp',
+  components: {
+    window,
+  },
+  data: () => ({
+	canBe1: {dicts: 'dicts},
+	canBe2: 'strings',
+	canBe3: ['arrays'],
+	canBe4: 5,
+	canBe5: 5.0,
+	canBe5: false,
+  }),
+  methods: {
+	function1(input) {
+	retun imput + 2;
+  },
+};
+</script>
+
+<style>
+
+.wallpaper {
+  background: #fff;
+}
+
+</style>
